a146c12110ecc02718a4cfea32cde6f56e293fc0
gposton <gposton1040@gmail.com>
2012-05-18 17:38:40 -0400

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Concrete::Application.config.secret_token = '1d95c6820fe12031b4453c764038232ff6c3d97d5363b975e4f60f8544785933df4b6445a44e02dd747fc3b04b7ac14ff1dc658a2a28805ec4b034639c5418a8'
